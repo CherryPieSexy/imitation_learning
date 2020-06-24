fix wrong testing with normalization

algorithms/policy_gradient.py
@@ -1,4 +1,5 @@
 # base class for policy gradient algorithms (A2C and PPO)
+import numpy as np
 import torch
 from torch.nn.utils import clip_grad_norm_
 
@@ -29,8 +30,6 @@ class PolicyGradientInference:
 
     def load(self, filename):
         checkpoint = torch.load(filename)
-        for k, v in checkpoint.items():
-            print(k)
         self.nn.load_state_dict(checkpoint['agent']['nn'])
         if 'obs_normalizer' in checkpoint:
             self.obs_normalizer = RunningMeanStd()
@@ -47,7 +46,7 @@ class PolicyGradientInference:
         """
         if self.obs_normalizer is not None:
             mean, var = self.obs_normalizer.mean, self.obs_normalizer.var
-            observations = (observations - mean) / (var + 1e-8)
+            observations = (observations - mean) / np.sqrt(var + 1e-8)
         with torch.no_grad():
             policy, _ = self.nn(torch.tensor(observations, dtype=torch.float32, device=self.device))
         action = self.distribution.sample(policy, deterministic)

train_scripts/test.py
@@ -1,18 +1,28 @@
 import torch
+import numpy as np
 
 from utils.init_env import init_env
 from algorithms.policy_gradient import PolicyGradientInference
 
 
-def play_episode(env, agent):
+def play_episode():
     episode_reward = 0.0
     obs, done = env.reset(), False
     while not done:
         env.render()
-        action = agent.act(obs, True)
+        action = agent.act(obs, deterministic=True)
         obs, reward, done, _ = env.step(action)
         episode_reward += reward
     return episode_reward
+
+
+def play_n_episodes():
+    episode_rewards = []
+    for i in range(n):
+        episode_reward = play_episode()
+        episode_rewards.append(float(episode_reward))
+        print(i, episode_reward)
+    print(np.mean(episode_rewards), np.std(episode_rewards))
 
 
 if __name__ == '__main__':
@@ -22,7 +32,8 @@ if __name__ == '__main__':
     )
     agent.eval()
     env = init_env('BipedalWalker-v3', 1)
-    agent.load(f'logs/Bipedal/new/22/checkpoints/epoch_0.pth')
-    play_episode(env, agent)
+    agent.load(f'logs/Bipedal/new/22/checkpoints/epoch_5.pth')
+    n = 20
+    play_n_episodes()
 
-    # env.close()
+    env.close()