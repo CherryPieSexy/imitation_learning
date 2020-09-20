02183abbca1b12121708a2d3fcbc41861c7ddb5e
CherryPieSexy <interga@post-hardcore.ru>
2020-09-20 17:35:38 +0300

    add Mario wrapper and config

---
+++ custom_wrappers/mario_wrapper.py
@@ -0,0 +1,23 @@
+class MarioWrapper:
+    def __init__(self):
+        self.prev_lives = 2
+        self.prev_score = 0
+
+    def step(self, observation, reward, done, info):
+        if info['lives'] < self.prev_lives:
+            done = True
+            reward -= 50
+
+        if info.get('flag_get', None):
+            done = True
+            reward += 100
+
+        reward += (info['score'] - self.prev_score) / 10.0
+        self.prev_score = info['score']
+
+        return observation, reward / 10.0, done, info
+
+    def reset(self, observation):
+        self.prev_lives = 2
+        self.prev_score = 0
+        return observation
