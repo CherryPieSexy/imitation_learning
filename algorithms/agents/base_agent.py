fix Bernoulli distribution and KL, change Mario wrapper and config, fix checkpoints loading

algorithms/agents/base_agent.py
@@ -34,8 +34,8 @@ class AgentInference:
         if self.distribution_with_params:
             self.distribution.load_state_dict(state['distribution'])
 
-    def load(self, filename):
-        checkpoint = torch.load(filename)
+    def load(self, filename, **kwargs):
+        checkpoint = torch.load(filename, **kwargs)
         agent_state = checkpoint['agent']
         self.load_state_dict(agent_state)
         if 'obs_normalizer' in checkpoint:
@@ -158,8 +158,8 @@ class AgentTrain:
             state_dict['reward_normalizer'] = reward_normalizer.state_dict()
         torch.save(state_dict, filename)
 
-    def load(self, filename):
-        checkpoint = torch.load(filename)
+    def load(self, filename, **kwargs):
+        checkpoint = torch.load(filename, **kwargs)
         agent_state = checkpoint['agent']
         self.load_state_dict(agent_state)
         # self.nn.load_state_dict(checkpoint['agent']['nn'])

algorithms/kl_divergence.py
@@ -14,7 +14,7 @@ def _d_kl_categorical(p, q):
 
 def _d_kl_bernoulli(p, q):
     p_probs = torch.sigmoid(p)
-    q_probs = torch.sigmoid(q)
+    q_probs = torch.clamp(torch.sigmoid(q), 0.001, 0.999)
     t1 = p_probs * (p_probs / q_probs).log()
     # noinspection PyTypeChecker,PyUnresolvedReferences
     t2 = (1.0 - p_probs) * ((1.0 - p_probs) / (1.0 - q_probs)).log()

trainers/on_policy.py
@@ -91,9 +91,9 @@ class OnPolicyTrainer(BaseTrainer):
         checkpoint = torch.load(filename)
         self._agent_online.load_state_dict(checkpoint['agent'])
         self._agent_train.load_state_dict(checkpoint['agent'])
-        if 'obs_normalizer' in checkpoint:
+        if 'obs_normalizer' in checkpoint and self._obs_normalizer is not None:
             self._obs_normalizer.load_state_dict(checkpoint['obs_normalizer'])
-        if 'reward_normalizer' in checkpoint:
+        if 'reward_normalizer' in checkpoint and self._reward_normalizer is not None:
             self._reward_normalizer.load_state_dict(checkpoint['reward_normalizer'])
 
     @staticmethod

utils/batch_crop.py
@@ -6,7 +6,11 @@ def batch_crop(tensor, pad_size=4):
     # tensor is assumed to have [t, b, c, h, w] shape
     time, batch, c, h, w = tensor.size()
     n = time * batch
-    cropped = torch.zeros(n, c, h, w)
+    cropped = torch.zeros(
+        n, c, h, w,
+        dtype=torch.float32,
+        device=tensor.device
+    )
     # fun.pad(...) can only work with 4-d tensors => need to unsqueeze and squeeze back
     padded = tensor.view(n, c, h, w)
     padded = fun.pad(padded, [pad_size] * 4, mode='replicate')