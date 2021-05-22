33f73a5f4c0427e2a76f3b11fc89d14e6f64a3d3
CherryPieSexy <interga@post-hardcore.ru>
2021-05-22 10:26:00 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='torch_rl',
+    version='0.1.0',
+    description='My awesome RL & IL library.',
+    url='https://github.com/CherryPieSexy/imitation_learning',
+    packages=['framework_name'],
+    install_requires=['gym', 'numpy', 'scipy', 'six', 'torch', 'tqdm'],
+)
