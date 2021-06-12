061ce8516a0b8bdb07dad934587e9673f54a534d
CherryPieSexy <interga@post-hardcore.ru>
2021-06-12 18:26:38 +0300

    update sutup for doom, add setup for vast

--- configs/doom/setup.sh
+++ configs/doom/setup.sh
@@ -1,6 +1,6 @@
-sudo apt install \
+apt install \
 cmake libboost-all-dev libsdl2-dev libfreetype6-dev \
 libgl1-mesa-dev libglu1-mesa-dev libpng-dev libjpeg-dev \
 libbz2-dev libfluidsynth-dev libgme-dev libopenal-dev \
 zlib1g-dev timidity tar nasm
-pip install vizdoom
+pip install opencv-python vizdoom
