f4f4cad73192d628ddca59e3c464a83c858bf14c
CherryPieSexy <interga@post-hardcore.ru>
2021-08-04 16:34:04 +0300

    upd setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='cherry_rl',
     version='0.1.0',
     description='My awesome RL & IL library.',
     url='https://github.com/CherryPieSexy/imitation_learning',
-    packages=['cherry_rl'],
+    packages=find_packages(),
     install_requires=['gym', 'numpy', 'scipy', 'six', 'torch', 'tensorboard', 'tqdm'],
 )
